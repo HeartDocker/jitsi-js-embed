4124b644be1b840b7378c906b82c2b3b650ea6cf
mirusu400 <mirusu400@naver.com>
2022-07-04 02:01:08 +0900

    Initial commit

---
+++ src/JitsiView.js
@@ -0,0 +1,22 @@
+import JitsiMeetJS from '@solyd/lib-jitsi-meet';
+import { useEffect } from 'react';
+// Re-implement of lib-jitsi-meet
+// https://github.com/solydhq/lib-jitsi-meet/blob/master/doc/example/example.js
+const initOptions = {
+    disableAudioLevels: true,
+}
+var connection = null;
+
+const JitsiView = () => {
+    useEffect(() => {
+        JitsiMeetJS.init(initOptions);
+        connection = new JitsiMeetJS.JitsiConnection(null, null, {
+            disableAudioLevels: true,
+        });
+    })
+    return (
+        <div>
+            Hello world!
+        </div>
+    )
+}
